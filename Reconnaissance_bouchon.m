bcf2ff02fa170a1fe6259683cb8eda374b93841b
Ranekey <78801061+Ranekey@users.noreply.github.com>
2024-04-09 18:01:54 +0200

    Add files via upload

---
+++ Reconnaissance_bouchon.m
@@ -0,0 +1,35 @@
+clear all
+close all
+
+%Affiche image originale
+image_rgb_wassim = imread("IMG_wassim.jpg");
+figure;
+imshow(image_rgb_wassim);title('image originale');
+
+%Affiche image ndg
+image_wassim_nvg = rgb2gray(image_rgb_wassim);
+figure;
+imshow(image_wassim_nvg);title('image nvg');
+
+%Effectue un filtre moyenneur sur l'image
+img_moy = filtreM(image_wassim_nvg,3); 
+figure;
+imshow(img_moy);title('image moyenne');
+
+%seuillage
+seuil = 90 ;
+binary = 255*(img_moy<seuil);
+figure;
+imshow(binary);title('seuillage')
+
+%Dilation
+img_dil = filtreDilat(binary,5);
+img_dil2 = filtreDilat(img_dil,3);
+figure;
+imshow(img_dil2);title('image dilater')
+
+
+%Segmentation
+img_seg = segmentons(img_dil2);
+figure;
+imshow(img_seg);title('image segmenter')
---
+++ filtreM.m
@@ -0,0 +1,20 @@
+% Filtre moyenneur taille x taille sur image nvg
+function imgresult = filtreM (img,taille)
+% copier img dans imgresult
+imgresult = img;
+% calcul nblig et nbcol de l'image
+nblig = size(img,1);
+nbcol = size(img,2);
+
+% calcul du decalage pour se déplacer dans le filtre
+dec = (taille-1)/2;
+
+% je me déplace dans toute l'image pour filtrer
+for lig=dec+1 : nblig-dec
+  for col=dec+1:nbcol-dec
+    moy = mean(mean(img(lig-dec:lig+dec,col-dec:col+dec)));
+    imgresult(lig,col) = moy;
+  end
+end
+
+
